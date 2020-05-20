ec249ef11505ca50abb44e80e49cd135bcb5b0f7
Matt Farncombe <matt@farncombe.co>
2020-05-20 00:42:10 +0000

    Automatically backed up by Learn

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -16,8 +16,6 @@ class String
 
   def count_sentences
     sentence_count = self.delete(",").squeeze!
-    puts sentence_count
-    
-      sentence_count.delete(" ").split(/[". ", "! ", "? "]/).length
+    sentence_count.delete(" ").split(/[". ", "! ", "? "]/).length
   end
 end
